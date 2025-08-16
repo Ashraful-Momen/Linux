9ddf364c5afcd198f8eab34274fa307968ae2f17
Md.Ashraful Momen <34467735+Ashraful-Momen@users.noreply.github.com>
2025-08-16 16:31:24 +0600

    Create Hello.cpp

---
+++ Problem_Fix/C++/1. Run Program with gcc , output.txt , input.txt, hello.cpp/Hello.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int num;
+    while (cin >> num) {       // read numbers until EOF
+        cout << num << endl;   // print each number
+    }
+    return 0;
+}
